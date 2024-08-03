434542f3d81cd2de85b7b8f89ee8d218976a6311
ayarn <iamayarn2001@gmail.com>
2024-08-03 21:56:52 +0530

    index.js changed

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const socketio = require("socket.io");
 const io = socketio(server);
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
